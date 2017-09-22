2c49aad4eb5bddced44021675ac1d8d644598c58
taphp <tdtabc@gmail.com>
2017-09-22 16:54:07 +0700

    afasf

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.get('/webhook', function(req, res) {
   res.send('Error, wrong validation token');
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "0.0.0.0");
 
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
