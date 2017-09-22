5fea3229e3bed62b702bdbf6dda7239f0e8a2cdd
taphp <tdtabc@gmail.com>
2017-09-22 18:01:59 +0700

    asdsafdf

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function sendMessage(senderId, message) {
   });
 }
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
 server.listen(app.get('port'), app.get('ip'), function() {
